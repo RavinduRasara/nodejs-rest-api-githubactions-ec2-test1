cf866540e414eae38e147253f8b54298b25130aa
RavinduRasara <r3ravi20@gmail.com>
2025-10-18 22:16:59 +0530

    added new test route and Updated product message

--- server.js
+++ server.js
@@ -20,9 +20,19 @@ app.use("/api/users", userRoutes);
 
 app.use("/api/products", (req, res) => {
   return res.status(200).json({
-    message: "This is new feature change, a new route for products samin",
+    message: "This is new product massages",
   });
 });
+
+// simple test route for CI/CD
+app.use("/api/test", (req, res) => {
+  return res.status(200).json({
+    message: "Hello! This is a test route for CI/CD",
+    timestamp: new Date(),
+  });
+});
+
+
 
 //error handler
 app.use(errorHandler);
